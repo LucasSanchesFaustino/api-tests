075e264560cb446fe16764af1c06436f38d9ff56
Lucas <lucassanchesfaustino3@hotmail.com>
2024-03-10 14:26:48 -0300

    Initial commit

---
+++ src/test/java/CalculatorControllerTest.java
@@ -0,0 +1,67 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+
+class CalculatorControllerTest {
+
+	@BeforeAll
+	public static void setUp() {
+		RestAssured.baseURI = "http://localhost:8080";
+	} 
+
+	@Test
+	public void sumControllerTest() {
+		RestAssured.given()
+			.queryParam("number1", 2)
+			.queryParam("number2", 2)
+		.when()
+			.basePath("calculator")
+			.get("sum")		
+		.then()
+			.statusCode(200)
+			.body(is("4.0"));
+	}
+
+	@Test
+	public void subtractControllerTest() {
+		RestAssured.given()
+			.queryParam("number1", 2)
+			.queryParam("number2", 2)
+		.when()
+		    .basePath("calculator")
+			.get("subtract")
+		.then()
+			.statusCode(200)
+			.body(is("0.0"));
+	}
+	
+	@Test
+	public void multiplyControllerTest() {
+		RestAssured.given()
+			.queryParam("number1", 2)
+			.queryParam("number2", 2)
+		.when()
+			.basePath("calculator")
+			.get("multiply")
+		.then()
+			.statusCode(200)
+			.body(is("4.0"));
+	}
+	
+	@Test
+	public void divideControllerTest() {
+		RestAssured.given()
+			.queryParam("number1", 2)
+			.queryParam("number2", 2)
+		.when()
+			.basePath("calculator")
+			.get("divide")
+		.then()
+			.statusCode(200)
+			.body(is("1.0"));
+	}
+}
